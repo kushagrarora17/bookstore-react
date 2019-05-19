645aefdea1827c8b4bc20e9992d5c426931ae4c7
Kushagr <kushagrarora.17@gmail.com>
2019-05-19 14:48:36 +0530

    initial setup

---
+++ precache-manifest.2aeaab0f2ae1eafd46818d17fd4a8651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9161d1a78343e87266768c028f173740",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be17a874c3e9df840561",
+    "url": "./static/css/main.dc1296c6.chunk.css"
+  },
+  {
+    "revision": "962fd731ea319f7bff31",
+    "url": "./static/js/2.e045ddc6.chunk.js"
+  },
+  {
+    "revision": "be17a874c3e9df840561",
+    "url": "./static/js/main.1062f48f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "456bfb5e7008784ad5eea85981844486",
+    "url": "./static/media/1907.456bfb5e.png"
+  }
+]);
